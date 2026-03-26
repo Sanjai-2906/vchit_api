58640cfb94c5a7dff47ee525bb18d67f741f9fed
Sanjai <sanjaipaulraj03@gmail.com>
2026-03-26 16:27:55 +0530

    rand added

--- src/handlers/due_amount.rs
+++ src/handlers/due_amount.rs
@@ -1,14 +1,24 @@
 use axum::Json;
+use rand::prelude::*;
 
 use crate::models::{GetDueModel,DueModel};
 
 pub async fn get_due_amount(Json(data): Json<GetDueModel>) -> Json<DueModel>{
     println!("Get Due Model : {:?}",data);
+    let mut rng1 = rand::rng();
+    let mut rng2 = rand::rng();
+
+    let mut pend_due: Vec<i32> = (200..500).collect();
+    let mut current_due: Vec<i32> = (250..700).collect();
+    pend_due.shuffle(&mut rng1);
+    current_due.shuffle(&mut rng2);
 
     let res =  DueModel{
-        pending_due: 500.00,
-        current_due: 450.00
+        // pending_due: 500.00,
+        pending_due: *pend_due.choose(&mut rng1).unwrap() as f32,
+        // current_due: 450.00
+        current_due: *current_due.choose(&mut rng2).unwrap() as f32,
     };
-    
+
     return Json(res);
 }
